b6617dd4be9eacbb3c0185a4b6f75ab1d854cb27
ouangyb3 <riunr31@126.com>
2022-02-14 20:43:38 +0800

    修正可见面积不准确问题。

--- MobileTracking/Viewability/MMA_AdCalSize.m
+++ MobileTracking/Viewability/MMA_AdCalSize.m
@@ -143,12 +143,12 @@
     if (adNode == nil) {
         return;
     }
-    if (leftNode == nil) {
-        return;
-    }
-    if (rightNode == nil) {
-        return;
-    }
+//    if (leftNode == nil) {
+//        return;
+//    }
+//    if (rightNode == nil) {
+//        return;
+//    }
     if (line.left == adNode.left && line.right == adNode.right) {
         adNode.coveredNum += line.isTop ? 1 : -1;
 //        计算当前所有的区间的覆盖面积
