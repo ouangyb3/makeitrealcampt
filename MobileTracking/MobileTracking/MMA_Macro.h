7387cd76815c7c577add41c72552c21be3fb99bf
ouangyb3 <riunr31@126.com>
2022-04-07 03:52:59 +0800

    v2.1.6 更新sdkconfig.xml中girdsum配置参数

--- MobileTracking/MobileTracking/MMA_Macro.h
+++ MobileTracking/MobileTracking/MMA_Macro.h
@@ -9,7 +9,7 @@
 #ifndef MobileTracking_Macro_h
 #define MobileTracking_Macro_h
 
-#define MMA_SDK_VERSION @"V2.1.5"
+#define MMA_SDK_VERSION @"V2.1.6"
 
 #define NOTIFICATION_VB @"viewability_notification"
 #define NOTIFICATION_EXPOSE @"expose_notification"
